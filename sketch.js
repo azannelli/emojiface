2e03c1ab6db0b3c49508edf6628940b8e920a91e
Alexander Zannelli <alexander.zannelli.20@cnu.edu>
2022-01-13 17:12:11 -0500

    added code to sketch.js so that now the output from the webcam is more pixelated by only displaying a certain number of pixels using the gridsize variable

--- sketch.js
+++ sketch.js
@@ -3,11 +3,27 @@ let video;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  video = createCapture(VIDEO)
+  video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 }
 
 function draw() {
-  image(video, 0,0);
+  background(255);
+
+  let gridSize = 50;
+
+  video.loadPixels();
+  for (let y=0; y<video.height; y+= gridSize) {
+    for (let x=0; x<video.width; x+= gridSize) {
+      let index = (y * video.width + x) * 4;
+      let r = video.pixels[index];
+      let g = video.pixels[index+1];
+      let b = video.pixels[index+2];
+
+      fill(r,g,b);
+      noStroke();
+      rect(x,y,gridSize,gridSize);
+    }
+  }
 }
