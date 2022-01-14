bef7669d64f81b6f078919841d5418dc0c72cb55
Alexander Zannelli <alexander.zannelli.20@cnu.edu>
2022-01-13 21:18:53 -0500

    added more colors and made it so that mouse location effects the resolution of the output

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function setup() {
 
 function colors(r,g,b) {
   let c = [r,g,b];
-  const emojis = ["\uD83C\uDF11","\uD83D\uDE21","\uD83C\uDF4A","\uD83D\uDE42","\uD83C\uDF40","\uD83D\uDCA7","\uD83D\uDCAD"];
-  const colors = [[0,0,0],[255,0,0],[255,125,0],[255,255,0],[0,255,0],[0,0,255],[255,255,255]];
+  const emojis = ["\uD83D\uDDA4","\uD83D\uDE21","\uD83C\uDF4A","\uD83D\uDE42","\uD83C\uDF40","\uD83D\uDCA7","\uD83D\uDCAD","\uD83D\uDD90\uD83C\uDFFB","\uD83D\uDD90\uD83C\uDFFC","\uD83D\uDD90\uD83C\uDFFD", "\uD83D\uDD90\uD83C\uDFFE", "\uD83D\uDD90\uD83C\uDFFF"];
+  const colors = [[0,0,0],[255,0,0],[255,125,0],[255,255,0],[0,255,0],[0,0,255],[255,255,255],[247,215,196],[216,176,148],[187,145,103],[142,86,46],[97,61,48]];
   var difference = 1000;
   var index = 0;
   for (i = 0; i < colors.length; i++) {
@@ -28,9 +28,7 @@ function colors(r,g,b) {
 
 function draw() {
   background(255);
-
-  let gridSize = 50;
-
+  let gridSize = int(map(mouseX,0,width,10,100));
   video.loadPixels();
   for (let y=0; y<video.height; y+= gridSize) {
     for (let x=0; x<video.width; x+= gridSize) {
@@ -39,7 +37,7 @@ function draw() {
       let g = video.pixels[index+1];
       let b = video.pixels[index+2];
       let emojitext = colors(r,g,b);
-      textSize(20);
+      textSize(gridSize);
       if ((r >= b) && (r>=g)) {
         text(emojitext,x,y);
       } else if ((g > b) && (g > b)) {
